e439311d5ebbc8f47dbdf74d5bc2928e44223721
Gastti <gutigas96@gmail.com>
2023-06-23 14:35:55 -0300

    uploading files

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,19 +1,24 @@
-const custombutton = document.querySelectorAll('.custombutton');
-const modals = document.querySelectorAll('.modal');
+const custombutton = document.querySelectorAll(".custombutton");
+const modals = document.querySelectorAll(".modal");
 
 custombutton.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        modals[index].classList.add('active');
-    })
-})
+  button.addEventListener("click", (e) => {
+    if (
+      !e.currentTarget.classList.value.includes("confirmation") &&
+      !e.currentTarget.classList.value.includes("giftlist")
+    ) {
+      modals[index].classList.add("active");
+    }
+  });
+});
 
 modals.forEach((modal, index) => {
-    modal.addEventListener('click', (e) => {
-        e.stopPropagation();
-        if (e.target === modal) {
-            modal.classList.remove('active');
-        } else if (e.target === modalContent) {
-            e.stopPropagation();
-        }
-    })
-})
+  modal.addEventListener("click", (e) => {
+    e.stopPropagation();
+    if (e.target === modal) {
+      modal.classList.remove("active");
+    } else if (e.target === modalContent) {
+      e.stopPropagation();
+    }
+  });
+});
